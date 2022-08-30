9fe0a8272da9a5fd79474589c876dd7ee0fbbc63
AlexSlayer69 <akulvjha2808@gmail.com>
2022-08-31 03:16:24 +0530

    Homepage hero section

---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Homepage.module.css';
+import About from '../About/about';
+
+function Home() {
+    return(
+        <div className={styles.container}>
+            <div className={styles.bg}>
+                <div className = {styles.im}></div>
+                <div className = {styles.vert}></div>
+                <h1 className={styles.heading}>CAMPUS<br/>AMBASSADOR<br/>PROGRAM</h1>
+            </div>
+        
+        <About/>
+        
+        </div>
+    );
+
+}
+
+export default Home;
