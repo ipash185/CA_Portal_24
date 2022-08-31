7b0cdf8a1187f1b4e808b953da26083b9d129e8c
AlexSlayer69 <akulvjha2808@gmail.com>
2022-09-01 02:22:02 +0530

    About page

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -1,10 +1,21 @@
-
+import styles from './about.module.css';
 
 function About() {
     return(
-
-        <div></div>
-
+        <div className={styles.main}>
+            <h1>ABOUT US</h1>
+            <div className={styles.box}>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a cursus velit, sed posuere diam. 
+                Duis non lacus eget odio malesuada sollicitudin. In consequat sem id elit lacinia, ac imperdiet 
+                mi semper. Vestibulum ac diam luctus, venenatis nunc vitae, ornare quam. Vestibulum tincidunt iaculis  
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a cursus velit, sed posuere diam. Duis non lacus
+                 eget odio malesuada sollicitudin. In consequat sem id elit lacinia, ac imperdiet mi semper. Vestibulum ac diam 
+                 luctus, venenatis nunc vitae, ornare quam. Vestibulum tincidunt iaculis  Lorem ipsum dolor sit amet, consectetur 
+                 adipiscing elit. Nunc a cursus velit, sed posuere diam. Duis non lacus eget oudin. In consequat sem id elit lacinia, 
+                 ac imperdiet mi semper. Vestibulum ac diam luctus
+                </p>
+            </div>
+        </div>
     );
 }
 
