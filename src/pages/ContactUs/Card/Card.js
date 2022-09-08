3d19c8b57477504ba96692ccc9a9fd27df5e1ed3
souvikm2002 <souvikm110902@gmail.com>
2022-09-08 21:30:58 +0530

    desings

--- src/pages/ContactUs/Card/Card.js
+++ src/pages/ContactUs/Card/Card.js
@@ -9,9 +9,11 @@ function Card(props) {
 
     return (
         <div className={styles.card}>
-            <div className={styles.circle}></div>
+            <div className={styles.circle}>
+                <img src={props.img} alt={props.name}></img>
+            </div>
             <div className={styles.name}>{props.name}</div>
-            <div className={styles.job}>J{props.job}</div>
+            <div className={styles.job}>{props.job}</div>
             <div className={styles.icons}><a href={email}><MdEmail /></a>&nbsp;<a href={props.fb}><MdFacebook /></a>&nbsp;<a href={props.li}><AiFillLinkedin /></a></div>
             <div className={styles.num}>{props.num}</div>
         </div>
