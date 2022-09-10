41ff571464e6a88d7a0f3e8e4d555140c526daec
souvikm2002 <souvikm110902@gmail.com>
2022-09-11 04:05:46 +0530

    done

--- src/pages/Incentives/incomp.js
+++ src/pages/Incentives/incomp.js
@@ -5,7 +5,7 @@ function Incomp(props){
     return(
         <div className={styles.elem}>
             <img className={styles.circle} src={props.img} ></img>
-            <p className={styles.para}><b style={{fontSize: '1.5rem'}}> {props.text}</b></p>
+            <p className={styles.para}><b style={{fontSize: '1.8rem'}}> {props.text}</b></p>
         </div>
     );
 }
