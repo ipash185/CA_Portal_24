c15fc7b9604e0391557be572ced0e0f082f40ada
Chaitanya-Srinivas <111225128+Chaitanya-Srinivas@users.noreply.github.com>
2022-10-05 04:40:48 +0530

    changes made

--- src/pages/Dashboard/DashBoard.js
+++ src/pages/Dashboard/DashBoard.js
@@ -5,11 +5,17 @@ import Navbar from "../../components/navbar/navbar";
 const DashBoard = () => {
   return (
     <div>
-      <Navbar />
       <div className={styles.TotalDiv}>
-        <div className={styles.LeftDiv}></div>
-        <div classname={styles.Head1}>LIVE EVENTS</div>
-        <div className={styles.Div1}></div>
+        <Navbar />
+        <div className={styles.LeftDiv}>
+            <div className={styles.NameOf}>NAME OF THE CANDI<br />KTJ ID : KTJ229856</div>
+            <div className={styles.RewardP}></div>
+        </div>
+        <div classname={styles.TopDiv}></div>
+        <div className={styles.Div1}>
+          <div className={styles.Over1}>Hello</div>
+        </div>
+
         <div className={styles.Div1}></div>
         <div className={styles.Div1}></div>
         <div className={styles.Div2}></div>
