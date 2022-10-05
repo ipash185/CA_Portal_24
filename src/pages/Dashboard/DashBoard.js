1e8c7ab395d0c1c0cd282157a56393e24ed2603d
souvikm2002 <souvikm110902@gmail.com>
2022-10-05 15:05:12 +0530

    dashboard

--- src/pages/Dashboard/DashBoard.js
+++ src/pages/Dashboard/DashBoard.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./Dash.module.css";
 import Navbar from "../../components/navbar/navbar";
+import Api from '../../API/Api';
 
 const DashBoard = () => {
+
+  const [auth, setAuth] = useState(false)
+  const [user, setUser] = useState("");
+  useEffect(() => {
+
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem('token') }
+    };
+
+
+
+    Api.get(`/user/login_check`, requestOptions).then((res) => {
+
+      // console.log(res.data);
+      setUser(res.data.user)
+      setAuth(true);
+    }).catch((err) => {
+      console.log(err);
+      setAuth(false);
+    })
+
+
+  }, [])
+
+
   return (
     <div>
       <div className={styles.TotalDiv}>
-        <Navbar />
+        <Navbar show={auth}/>
         <div className={styles.LeftDiv}>
             <div className={styles.NameOf}>NAME OF THE CANDI<br />KTJ ID : KTJ229856
             <br /><br /><br /><br /><br />SHARE TO EARN MORE REWARD POINTS</div>
