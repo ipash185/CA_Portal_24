2736b33a37255d69980094d46882f92373394be9
Chaitanya-Srinivas <111225128+Chaitanya-Srinivas@users.noreply.github.com>
2022-10-10 10:26:08 +0530

    Hovering done

--- src/pages/Dashboard/Card/Card.js
+++ src/pages/Dashboard/Card/Card.js
@@ -4,7 +4,17 @@ function Card(props) {
     return (
         <div className={styles.card}>
             <div className={styles.circle}>
-                <img src={props.img} alt={props.name} height="100%" width="100%"></img>
+                <img className={styles.ImgB} src={props.img} alt={props.name} height="100%" width="100%"></img>
+                <div className={styles.Overlap}>
+                <h1 className={styles.Head}>{props.Heading}</h1>
+                <div className={styles.Icons}>
+              <img className={styles.Image} src={props.img1} />
+              <img className={styles.Image} src={props.img2} />
+              <img className={styles.Image} src={props.img3} />
+              <img className={styles.Image} src={props.img4} />
+              <img className={styles.Image} src={props.img5} />
+            </div>
+            </div>
             </div>
             
         </div>
