7e887eb81fcf470c5be4d533802ba19b8e95ec1f
Deep Sekhar Ghosh <73838737+deep-sekhar@users.noreply.github.com>
2022-12-03 20:37:54 +0530

    Update Api.js

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const devUrl = "http://localhost:5000"
-const productionUrl = "https://ca-backend-api.herokuapp.com"
+const productionUrl = "https://ktj.in/caApi"
 
 const Api = axios.create({
  baseURL: productionUrl,
