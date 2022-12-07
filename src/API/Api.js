8d8abcc0f980b9235edd85d964a2cffd5026f45d
Deep Sekhar Ghosh <73838737+deep-sekhar@users.noreply.github.com>
2022-12-07 17:38:33 +0530

    Update Api.js

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const devUrl = "http://localhost:5000"
-const productionUrl = "https://cap-backend-production-2e18.up.railway.app"
+const productionUrl = "https://cap-backend-production-2e18.up.railway.app/caApi"
 
 const Api = axios.create({
  baseURL: productionUrl,
