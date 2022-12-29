afcd4bf11e2ec47ada7074b4d0fd51aaab3d38a0
Deep <sekharsmemories@gmail.com>
2022-12-29 16:17:37 +0530

    Co-authored-by: Souvik Mondal <souvikm2002@users.noreply.github.com>

--- src/API/Api.js
+++ src/API/Api.js
@@ -4,7 +4,7 @@ const devUrl = "http://localhost:5000/caApi"
 const productionUrl = "https://ca-backend.onrender.com/caApi"
 
 const Api = axios.create({
- baseURL: devUrl,
+ baseURL: productionUrl,
 });
 
 export default Api;
